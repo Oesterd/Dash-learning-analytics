48209e578e18bd2d594efa167bd700c1201dd1a5
Oesterd <pedrocattany@proton.me>
2024-01-11 19:13:15 -0300

    Multiple excel files

--- src/app.py
+++ src/app.py
@@ -1,9 +1,14 @@
 # Importando as bibliotecas
+
+import io
 from io import BytesIO
+import base64
+import datetime
+
+
 import matplotlib
 matplotlib.use('agg')
 import matplotlib.pyplot as plt
-import base64
 import openpyxl
 
 
@@ -15,7 +20,7 @@ import pandas as pd
 import numpy as np
 import openpyxl
 
-from dash import Dash, html, dcc, dash_table, Input, Output, callback
+from dash import Dash, html, dcc, dash_table, Input, Output, State, MATCH, no_update, callback
 from dash.dash_table.Format import Format, Scheme
 
 # Iniciando o aplicativo
@@ -48,11 +53,11 @@ df
 
 
 # Formatação dos números
-
 numformat = Format(precision=2, scheme=Scheme.fixed, decimal_delimiter=',')
 renformat = Format(precision=0, scheme=Scheme.fixed, decimal_delimiter=',')
 
 
+# Separando os dados para criar os dicionários
 dfmed = df.iloc[:, 5:8]
 dftext = df.iloc[:, 1:4]
 dfend = df.iloc[:, 8:10]
@@ -81,68 +86,207 @@ print(op2)
 
 
 # Layout do dash
-app.layout = \
-html.Div([
-    html.Div([
-        dash_table.DataTable(  # Tabela de dados
-            id='datatable_id',
-            columns=dictaln + dicttext + dictren + dictmed + dictend,
-            style_cell={'textAlign': 'center'},
-            sort_action='native',
-            sort_mode='single',
-            filter_action='native',
-            page_size=8,
-            data=df.to_dict('records')
-        )
-    ]),
-
-    # Menus de dropdown
-    html.Div([
-        html.Div([
-            "Divisão por cores:",
-            dcc.Dropdown(id='dropdown', value='Nenhuma', options=op1, clearable=False),
+app.layout = html.Div([
+    html.H1('Dashboard of Multiple Excel Sheets', style={'textAlign': 'center'}),
+    dcc.Upload(
+        id='upload-data',
+        children=html.Div([
+            'Drag and Drop or ',
+            html.A('Select Files')
         ]),
+        style={
+            'width': '100%',
+            'height': '60px',
+            'lineHeight': '60px',
+            'borderWidth': '1px',
+            'borderStyle': 'dashed',
+            'borderRadius': '5px',
+            'textAlign': 'center',
+            'margin': '10px'
+        },
+        # Allow multiple files to be uploaded
+        multiple=True
+    ),
 
-        html.Div([
-            "Divisão por coluna:",
-            dcc.Dropdown(id='dropdown2', value='Nenhuma', options=op2, clearable=False),
-        ]),
-
-        html.Div([
-            "Tipo de gráfico:",
-            dcc.Dropdown(id='dropdown3', value='kde', options=[
-                {'label': 'KDE', 'value': 'kde'},
-                {'label': 'Histograma', 'value': 'hist'},
-                {'label': 'Cumulativo', 'value': 'ecdf'}], clearable=False),
-        ]),
-
-        html.Div([
-            "Tipo de agrupamento:",
-            dcc.Dropdown(id='dropdown4', value='layer', options=[
-                {'label': 'Normal', 'value': 'layer'},
-                {'label': 'Empilhar', 'value': 'stack'}], clearable=False),
-        ])
-    ], style={'display': 'flex', 'flexDirection': 'row', 'gap':50, 'flex':1}),
-
-    html.Div([
-        html.Img(id='matplot')  # Imagem
-    ])
-
-
-
+    html.Div(id='output-data-upload', children=[]),
 ])
+
+
+
+
+
+# Upload CSV and Excel sheets to the app and create the tables----------------------------------------------------------
+@app.callback(Output('output-data-upload', 'children'),
+              Input('upload-data', 'contents'),
+              State('upload-data', 'filename'),
+              State('upload-data', 'last_modified'),
+              State('output-data-upload', 'children'),
+              prevent_initial_call=True
+)
+def update_output(contents, filename, date, children):
+    # part of the code snippet is from https://dash.plotly.com/dash-core-components/upload
+    if contents is not None:
+        for i, (c, n, d) in enumerate(zip(contents, filename, date)):
+
+            content_type, content_string = contents[i].split(',')
+
+            decoded = base64.b64decode(content_string)
+            try:
+                if 'csv' in filename[i]:
+                    # Assume that the user uploaded a CSV file
+                    df = pd.read_csv(io.StringIO(decoded.decode('utf-8')))
+                elif 'xls' in filename[i]:
+                    # Assume that the user uploaded an excel file
+                    df = pd.read_excel(io.BytesIO(decoded))
+
+                # Create the tables and empty graphs
+                children.append(html.Div([
+                    html.H5(filename[i]),
+                        html.Div([
+                            dash_table.DataTable(  # Tabela de dados
+                                id={'type': 'dynamic-table',
+                                    'index': i},
+                                columns=dictaln + dicttext + dictren + dictmed + dictend,
+                                style_cell={'textAlign': 'center'},
+                                sort_action='native',
+                                sort_mode='single',
+                                filter_action='native',
+                                page_size=8,
+                                data=df.to_dict('records')
+                            ),
+
+                            # For debugging
+                            # html.Div('Raw Content'),
+                            # html.Pre(contents[i][0:200] + '...', style={
+                            #     'whiteSpace': 'pre-wrap',
+                            #     'wordBreak': 'break-all'
+                            # }),
+                            # html.Hr()
+                        ]),
+
+
+                        # # Menus de dropdown
+                        html.Div([
+                            html.Div([
+                                "Divisão por cores:",
+                                dcc.Dropdown(id={
+                                    'type': 'dropdown',
+                                    'index': i
+                                    }, value='Nenhuma', options=op1, clearable=False),
+                            ]),
+
+                            html.Div([
+                                "Divisão por coluna:",
+                                dcc.Dropdown(id={
+                                    'type': 'dropdown2',
+                                    'index': i
+                                }, value='Nenhuma', options=op2, clearable=False),
+                            ]),
+
+                            html.Div([
+                                "Tipo de gráfico:",
+                                dcc.Dropdown(id={
+                                    'type': 'dropdown3',
+                                    'index': i
+                                },
+                                    value='kde',
+                                    options=[
+                                    {'label': 'KDE', 'value': 'kde'},
+                                    {'label': 'Histograma', 'value': 'hist'},
+                                    {'label': 'Cumulativo', 'value': 'ecdf'}], clearable=False),
+                            ]),
+
+                            html.Div([
+                                "Tipo de agrupamento:",
+                                dcc.Dropdown(id={
+                                    'type': 'dropdown4',
+                                    'index': i
+                                },
+                                    value='layer',
+                                    options=[
+                                    {'label': 'Normal', 'value': 'layer'},
+                                    {'label': 'Empilhar', 'value': 'stack'}], clearable=False),
+                             ])
+                        ], style={'display': 'flex', 'flexDirection': 'row', 'gap':50, 'flex':1}),
+
+                        html.Div([
+                            html.Img(id={
+                            'type': 'dynamic-graph',
+                            'index': i
+                            })
+                        ])
+                ]))
+
+            except Exception as e:
+                print(e)
+                return html.Div([
+                    'There was an error processing this file.'
+                ])
+        return children
+    else:
+        return ""
+
+# html.Div([
+#     html.Div([
+#         dash_table.DataTable(  # Tabela de dados
+#             id='datatable_id',
+#             columns=dictaln + dicttext + dictren + dictmed + dictend,
+#             style_cell={'textAlign': 'center'},
+#             sort_action='native',
+#             sort_mode='single',
+#             filter_action='native',
+#             page_size=8,
+#             data=df.to_dict('records')
+#         )
+#     ]),
+#
+#     # Menus de dropdown
+#     html.Div([
+#         html.Div([
+#             "Divisão por cores:",
+#             dcc.Dropdown(id='dropdown', value='Nenhuma', options=op1, clearable=False),
+#         ]),
+#
+#         html.Div([
+#             "Divisão por coluna:",
+#             dcc.Dropdown(id='dropdown2', value='Nenhuma', options=op2, clearable=False),
+#         ]),
+#
+#         html.Div([
+#             "Tipo de gráfico:",
+#             dcc.Dropdown(id='dropdown3', value='kde', options=[
+#                 {'label': 'KDE', 'value': 'kde'},
+#                 {'label': 'Histograma', 'value': 'hist'},
+#                 {'label': 'Cumulativo', 'value': 'ecdf'}], clearable=False),
+#         ]),
+#
+#         html.Div([
+#             "Tipo de agrupamento:",
+#             dcc.Dropdown(id='dropdown4', value='layer', options=[
+#                 {'label': 'Normal', 'value': 'layer'},
+#                 {'label': 'Empilhar', 'value': 'stack'}], clearable=False),
+#         ])
+#     ], style={'display': 'flex', 'flexDirection': 'row', 'gap':50, 'flex':1}),
+#
+#     html.Div([
+#         html.Img(id='matplot')  # Imagem
+#     ])
+
+
+
+# ])
 
 
 
 
 #-------------------------------------------------------------------------------------
 @app.callback(
-    Output(component_id='matplot', component_property='src'),
-    Input(component_id='dropdown', component_property='value'),
-    Input(component_id='dropdown2', component_property='value'),
-    Input(component_id='dropdown3', component_property='value'),
-    Input(component_id='dropdown4', component_property='value'),
-    Input(component_id='datatable_id', component_property='derived_virtual_data')
+    Output(component_id={'type': 'dynamic-graph', 'index': MATCH}, component_property='src'),
+    Input(component_id={'type': 'dropdown', 'index': MATCH}, component_property='value'),
+    Input(component_id={'type': 'dropdown2', 'index': MATCH}, component_property='value'),
+    Input(component_id={'type': 'dropdown3', 'index': MATCH}, component_property='value'),
+    Input(component_id={'type': 'dropdown4', 'index': MATCH}, component_property='value'),
+    Input(component_id={'type': 'dynamic-table', 'index': MATCH}, component_property='derived_virtual_data'),
 )
 
 
@@ -150,14 +294,6 @@ html.Div([
 def matplot_html(drop1, drop2, drop3, drop4, rows):
 
 ## Criando o gráfico
-
-    # # Transformand as opções do dropdown 3 em valores aceitados pelo seaborn
-    # if drop3 == 'KDE':
-    #     pltkind = 'kde'
-    # elif drop3 == 'Histograma':
-    #     pltkind = 'hist'
-    # elif drop3 == 'Cumulativo':
-    #     pltkind = 'ecdf'
 
     # Modificando os dados de acordo com a filtragem do usuário
     dff = pd.DataFrame(rows)
@@ -185,7 +321,7 @@ def matplot_html(drop1, drop2, drop3, drop4, rows):
 
     return fig_matplot
 
-
+#
 
 
 # Run the app
